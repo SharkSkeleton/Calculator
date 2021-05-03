1cf31c3df850cc4c28f49858d1e47e51ab7ffee1
SharkSkeleton <mishamyagkiy3@gmail.com>
2021-05-03 22:37:26 +0300

    First (lite) calculator version with basic logic and just four operation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your a and b:");
+
+        System.out.print("a: ");
+        int a = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("b: ");
+        int b = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+
+        // Potential trouble (division by zero)
+        System.out.println("a / b = " + (a / b));
     }
 }
